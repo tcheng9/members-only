43ea2ed1bfcb6a3ae7cfbbda874c211762be7e46
Tommy C <tcheng789@gmail.com>
2022-10-06 16:59:30 -0500

    login is working sorta but hitting err_https_headers_sent error

--- members-only/controllers/userController.js
+++ members-only/controllers/userController.js
@@ -8,41 +8,7 @@ const passport = require('passport');
 const LocalStrategy = require("passport-local").Strategy;
 
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-  
-//Setting up passport
-passport.use(
-    new LocalStrategy((username, password, done) => {
-      User.findOne({ username: username }, (err, user) => {
-        if (err) { 
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false, { message: "Incorrect username" });
-        }
-        bcrypt.compare(password, user.password, (err, res) => {
-          if (res) {
-            // passwords match! log user in
-            return done(null, user)
-          } else {
-            // passwords do not match!
-
-            return done(null, false, { message: "Incorrect password" })
-          }
-        })
-        
-        return done(null, user);
-      });
-    })
-);
   
 // //GET request on signup form
 // exports.user_signup_get = function(req, res, next){
@@ -95,9 +61,6 @@ exports.user_signup_post = [
 
 ]
 
-// exports.user_login_post = function(req, res, next){
-//   res.send("test");
-// }
 
 //POST request for login form
 
@@ -115,96 +78,3 @@ exports.user_login_post = [
       next();
     },
 ];
-  // try{
-  //   const username = req.body.username;
-  //   const password = req.body.password;
-    
-  //   const item = User.find(u => u.username === username);
-
-  //   if (item){
-  //     passport.authenticate("local", (req, res, function(){
-  //       res.redirect('/');
-  //     }))
-  //   } else {
-  //     res.redirect('/signup');
-  //   }
-  // } catch (err) {
-  //   res.send(err);
-  // }
-// };
-
-// app.post('/login/password',
-// passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
-// function(req, res) {
-//   res.redirect('/~' + req.user.username);
-// });
-  
-
-  // if (!req.body.username) {
-  //   res.json({ success: false, message: "Username was not given" })
-  // }
-  // else if (!req.body.password) {
-  //     res.json({ success: false, message: "Password was not given" })
-  // }
-  // else {
-  //     passport.authenticate("local", function (err, user, info) {
-  //       successRedirect: "/",
-  //       failureRedirect: "/"  
-  //       // if (err) {
-          //     res.json({ success: false, message: err });
-          // }
-          // else {
-          //     if (!user) {
-          //         res.json({ success: false, message: "username or password incorrect" });
-          //     }
-          //     else {
-          //         const token = jwt.sign({ userId: user._id, username: user.username }, secretkey, { expiresIn: "24h" });
-          //         res.json({ success: true, message: "Authentication successful", token: token });
-          //     }
-          // }
-//       });
-//   }
-// }
-
-
-
-// exports.user_login_post = [
-//     body('username', 'username cannot be empty')
-//         .trim()
-//         .isLength({min:1})
-//         .escape(),
-//     body('password', 'password cannot be empty')
-//         .trim()
-//         .isLength({min:1})
-//         .escape(),
-//     async (req, res, next) => {
-//         const username = req.body.username;
-//         const password = req.body.password;
-        
-//         const item = User.find(u => u.username === username)
-        
-//         if (!item) {
-//             res.send("no user");
-            
-//         }
-
-//         // try {
-//         //     if(await bcrypt.compare(password, item.password)) {
-//         //         res.send("success")
-//         //     }
-//         // } catch {
-//         //     res.send('failure');
-//         // }
-        
-
-//         // if (!isValid){
-//         //     res.send("wrong password");
-            
-//         // } else {
-//         //     res.send("correct password");
-//         // }
-
-//         // res.send("here");
-
-//     }
-// ]
